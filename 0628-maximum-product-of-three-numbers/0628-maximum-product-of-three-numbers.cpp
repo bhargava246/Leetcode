5f5362a4729ea31f1e718fd9df0347ab2d7adc53
Himanshu Bhargava <113606456+bhargava246@users.noreply.github.com>
2025-12-19 12:37:35 +0530

    Time: 0 ms (100%), Space: 31.4 MB (94.78%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-      sort(nums.begin(),nums.end());
-    //   if(nums[nums.size()-1]<0){
-    //     return nums[0]*nums[1]*nums[2];
-    //   }else 
-      if (nums[1]>0 || nums[nums.size()-1]<0){
-        return nums[nums.size()-1]*nums[nums.size()-2]*nums[nums.size()-3];
-      }else{
-        nums[0]*=-1;
-        nums[1]*=-1;
-        return max(nums[0]*nums[1]*nums[nums.size()-1], nums[nums.size()-1]*nums[nums.size()-2]*nums[nums.size()-3]);
-      }
+      int max1 = -12312;
+     int max2 = -12312;
+     int max3 = -312121;
+     int min1 = 19999;
+     int min2 = 1999;
+     int value = 0;
+     for(int i=0;i<nums.size();i++){
+       value = nums[i];
+       if(value>max1){
+        max3 = max2;
+        max2=max1;
+        max1 = value;
+       }else if(max2<value){
+        max3 = max2;
+        max2 = value;
+       }else if(max3<value){
+        max3 = value;
+       }
+
+        if(min1>value){
+        min2 = min1;
+        min1 = value;
+       }else if(min2>value){
+        min2 = value;
+       }
+     }   
+     cout<<max1<<" "<<max2<<" "<<max3<<" "<<min1<<" "<<min2;
+    return max(max1*max2*max3,min1*min2*max1);
     }
 };
